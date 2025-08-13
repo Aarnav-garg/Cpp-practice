684bdee83b384d2a4a7983b3ba7290c72e788b1a
Aarnav-garg <71201422+Aarnav-garg@users.noreply.github.com>
2025-08-13 15:59:14 +0530

    Initial commit of basics

---
+++ Basics.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int x;
+    cin>>"Wow give your input if you want">>x;
+    cout<<"It's good to see you have entered a number only"<<x;
+}
